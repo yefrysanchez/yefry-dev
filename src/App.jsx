2fb30894882a29fe858d0e17fdedb0b61116247f
Yefry Sanchez <yefrymsp@gmail.com>
2024-03-06 14:55:21 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  // https://www.artjomzakoyan.com/
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
+}
+
+export default App;
